2326a40cec4adf2d06dd503c5237387246c804ae
D Gillen <declangillen@live.com>
2023-12-10 15:38:02 +0000

    maybe I hate puma too

--- config/puma.rb
+++ config/puma.rb
@@ -17,16 +17,16 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 #
 port ENV.fetch("PORT") { 3000 }
 #test to add https
-#ssl_bind '127.0.0.1', '443', {
-#	cert: '/usr/src/app/server.crt',
-#	key_pem: '/usr/src/app/privatekey.pem'
-#}
+ssl_bind '127.0.0.1', '443', {
+    cert: '/usr/src/app/server.crt',
+    key_pem: '/usr/src/app/privatekey.pem'
+}
 #bind "ssl://0.0.0.0:443?key=/usr/src/app/privatekey.pem&cert=/usr/src/app/server.crt"
 
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
